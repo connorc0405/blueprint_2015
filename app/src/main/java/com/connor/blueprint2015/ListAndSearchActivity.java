deb35474bc656b62b2363c1f1aff33886de79f8e
Gopaintman <schnitzemil@live.com>
2015-02-22 14:06:04 -0500

    SearchScreenLayoutFunctionalityUpdate

--- app/src/main/java/com/connor/blueprint2015/ListAndSearchActivity.java
+++ app/src/main/java/com/connor/blueprint2015/ListAndSearchActivity.java
@@ -2,6 +2,9 @@ package com.connor.blueprint2015;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
 
 /**
  * Created by Connor on 2/22/15.
@@ -12,6 +15,23 @@ public class ListAndSearchActivity extends Activity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.list_and_search_activity);
+        EditText searchBox = (EditText) findViewById(R.id.searchBox);
+        searchBox.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                
+            }
 
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
     }
+
 }
