241733db6a1f919d15b2a4df43e6b24ae9ef2a5c
Gopaintman <schnitzemil@live.com>
2015-02-22 14:31:27 -0500

    NumberPiker on Search Screen

--- app/src/main/java/com/connor/blueprint2015/ListAndSearchActivity.java
+++ app/src/main/java/com/connor/blueprint2015/ListAndSearchActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.widget.EditText;
+import android.widget.NumberPicker;
 
 /**
  * Created by Connor on 2/22/15.
@@ -14,12 +15,13 @@ public class ListAndSearchActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.list_and_search_activity);
         EditText searchBox = (EditText) findViewById(R.id.searchBox);
         searchBox.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-                
+
             }
 
             @Override
@@ -29,6 +31,16 @@ public class ListAndSearchActivity extends Activity{
 
             @Override
             public void afterTextChanged(Editable s) {
+
+            }
+        });
+        NumberPicker numberPicker = (NumberPicker) findViewById(R.id.searchRadiusPicker);
+        numberPicker.setValue(2);
+        numberPicker.setMaxValue(25);
+        numberPicker.setMinValue(0);
+        numberPicker.setOnValueChangedListener(new NumberPicker.OnValueChangeListener() {
+            @Override
+            public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
 
             }
         });
